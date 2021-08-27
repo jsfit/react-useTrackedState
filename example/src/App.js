b8fde4f56fb30caec77cdddcdc5221671669b209
MUHAMMAD ILYAS <mail.ilyas@yahoo.com>
2021-08-27 19:05:59 +0500

    two way binding added

--- example/src/App.js
+++ example/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import useTrackedState from "./rtp";
+import { useEffect } from "react"
 function App() {
+  const state = useTrackedState({ username: { value: 10   } })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{state.username.value}</h1>
+      <input ref={state.username} />
     </div>
   );
 }
